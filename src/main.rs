cea90fd49d4366b2fd115de2d097ddf5f4bce798
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-11-07 10:31:39 +0100

    Make it faster

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io::Read;
+use std::io::BufRead;
 use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let f = std::io::BufReader::with_capacity(
+    let mut f = std::io::BufReader::with_capacity(
         64 * 1024,
         std::fs::File::open(
             std::env::args()
@@ -17,33 +17,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut zeroes: u64 = 0;
     let mut total: u64 = 0;
     let mut t1 = std::time::Instant::now();
+    let t2 = std::time::Instant::now();
     const REFRESH_INTERVAL: std::time::Duration = std::time::Duration::from_secs(1);
 
     macro_rules! display_progress {
         () => {{
             let _ = write!(
                 stdout_lock,
-                "\r{}/{} ({:.2}%) ",
+                "\r{}/{} ({:.2}%) {}B/s ",
                 zeroes,
                 total,
                 zeroes as f64 / total as f64 * 100.0,
+                total.checked_div(t2.elapsed().as_secs()).unwrap_or(0),
             );
             let _ = stdout_lock.flush();
         }};
     }
 
-    for byte in f.bytes() {
-        let byte = byte?;
+    while let Ok(buffer) = f.fill_buf() {
+        let len = buffer.len();
 
-        total += 1;
-        if byte == 0 {
-            zeroes += 1;
+        if len == 0 {
+            break;
+        }
+
+        let (prefix, aligned, suffix) = unsafe { buffer.align_to::<u128>() };
+
+        total += len as u64;
+        if prefix.iter().all(|&x| x == 0)
+            && suffix.iter().all(|&x| x == 0)
+            && aligned.iter().all(|&x| x == 0)
+        {
+            zeroes += len as u64;
         }
 
         if t1.elapsed() >= REFRESH_INTERVAL {
             display_progress!();
             t1 = std::time::Instant::now();
         }
+
+        f.consume(len);
     }
 
     display_progress!();
